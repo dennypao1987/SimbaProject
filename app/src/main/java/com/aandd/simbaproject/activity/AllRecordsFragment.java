fdeb0223e2570c4718d9a32456adb88190d4fec6
denny.paolini <denny.paolini@declab.it>
2016-03-17 15:36:23 +0100

    First Commit af SimbaProject

--- app/src/main/java/com/aandd/simbaproject/activity/AllRecordsFragment.java
+++ app/src/main/java/com/aandd/simbaproject/activity/AllRecordsFragment.java
@@ -2,8 +2,7 @@ package com.aandd.simbaproject.activity;
 
 import java.io.File;
 
-import com.aandd.simbaproject.media.Play;
-import com.aandd.simbaproject.utility.FileName;
+import com.aandd.simbaproject.utility.FileUtil;
 import com.example.simbaproject.R;
 
 import android.app.Fragment;
@@ -12,8 +11,6 @@ import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 
 public class AllRecordsFragment extends Fragment{
@@ -39,32 +36,11 @@ public class AllRecordsFragment extends Fragment{
         
         return rootView;
     }
-	
-	public void setValues() {
-		String path = getPath();
-		File f = new File(path);        
-		File file[] = f.listFiles();
-		String[] File = new String[file.length];
-		for (int i=0; i < file.length; i++)
-		{
-			File[i] = file[i].getName();
-		}
-		values = File;
-	}
 
 //	@Override
 //	protected void onListItemClick(ListView l, View v, int position, long id) {
 //	    String item = (String) getListAdapter().getItem(position);
 //	    Play.mediaPlay(getPath() + "/" + item);
 //	}
-
-	public String getPath(){
-		String filepath = Environment.getExternalStorageDirectory().getPath();
-	    File file = new File(filepath, FileName.getAudioRecorderFolder());
-	    if (!file.exists()) {
-	        file.mkdirs();
-	    }	
-	    return file.getAbsolutePath();
-	}
 
 }
